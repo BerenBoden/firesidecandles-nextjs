b15c90550dc73f4418a2bec8d59437d8139daa81
berenboden <beren.boden@gmail.com>
2023-06-03 10:49:41 +1200

    added tailwind skeleton for /shop page with dummy data

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Featured from "@/components/page/Featured";
 import Showcase from "@/components/page/Showcase";
 import Special from "@/components/page/Special";
 import Footer from "@/components/common/Footer";
+import Products from "@/components/page/Products";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <div className="max-w-7xl mx-auto p-6 lg:px-8">
         <Featured />
         <Special />
+        <Products />
       </div>
       <Footer />
     </>
--- components/page/Featured.tsx
+++ components/page/Featured.tsx
@@ -3,12 +3,11 @@ const stats = [
   { id: 1, name: "Creators on the platform", value: "8,000+" },
   { id: 2, name: "Flat platform fee", value: "3%" },
   { id: 3, name: "Uptime guarantee", value: "99.9%" },
-  { id: 4, name: "Paid out to creators", value: "$70M" },
 ];
 export default function Featured() {
   return (
     <div className="bg-white">
-      <dl className="grid grid-cols-1 gap-x-4 gap-y-4 overflow-hidden text-center sm:grid-cols-2 lg:grid-cols-4">
+      <dl className="grid grid-cols-1 gap-x-4 gap-y-4 overflow-hidden text-center sm:grid-cols-2 lg:grid-cols-3">
         {stats.map((stat) => (
           <div className="">
             <Image
