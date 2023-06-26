34987e926f0dd79daa1fcce4c257f7ec5bd2b3bc
berenboden <beren.boden@gmail.com>
2023-06-26 18:43:00 +1200

    added redirect on successfull sign in

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/auth";
 
 export default async function Page() {
-  // const session = await getServerSession(authOptions);
-  // if (!session) redirect("/login");
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/login");
   return <div>page</div>;
 }
